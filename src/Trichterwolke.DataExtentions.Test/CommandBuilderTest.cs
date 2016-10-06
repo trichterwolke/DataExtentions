56c92fd1911afd39210b4153c228bbe3a020490c
Daniel <dvo@neos-it.de>
2016-10-06 10:27:57 +0200

    feat(All): Initial commit

---
+++ src/Trichterwolke.DataExtentions.Test/CommandBuilderTest.cs
@@ -0,0 +1,165 @@
+﻿namespace Trichterwolke.DataExtentions.Test
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Data.SqlClient;
+    using Xunit;
+
+    public class CommandBuilderTest
+    {
+        [Fact]
+        public void Append()
+        {
+            var target = new CommandBuilder(new SqlConnection());
+            target.Append(@"SELECT elephant ");
+            target.Append(@"FROM africa"); ;
+
+            var expected = @"SELECT elephant FROM africa";
+            var actual = target.CommandText;
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData(false, "SELECT elephant FROM africa")]
+        [InlineData(true, "SELECT elephant FROM africa WHERE sex = 'female'")]
+        public void Append_conditioanl(bool append, string expected)
+        {
+            var target = new CommandBuilder(new SqlConnection());
+            target.Append(@"SELECT elephant ");
+            target.Append(@"FROM africa");
+            target.Append(@" WHERE sex = 'female'", append);
+            
+            var actual = target.CommandText;
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void AppendParameterPhraseWhenNotNull_check_command_text()
+        {
+            var target = new CommandBuilder(new SqlConnection());
+            target.Append(@"SELECT elephant ");
+            target.Append(@"FROM africa ");
+            target.Append(@"WHERE sex = 'male' ");
+
+            target.AppendWithParameterWhenNotNull("AND born > @date", "@date", new DateTime(2016, 10, 03));
+
+            var expected = @"SELECT elephant FROM africa WHERE sex = 'male' AND born > @date";
+            var actual = target.CommandText;
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void AppendParameterPhraseWhenNotNull_check_command_text_parameter_null()
+        {
+            var target = new CommandBuilder(new SqlConnection());
+            target.Append(@"SELECT elephant ");
+            target.Append(@"FROM africa ");
+            target.Append(@"WHERE sex = 'male' ");
+
+            target.AppendWithParameterWhenNotNull("AND born > @date", "@date", (object)null);
+
+            var expected = @"SELECT elephant FROM africa WHERE sex = 'male' ";
+            var actual = target.CommandText;
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void AppendParameterPhraseWhenNotNull_check_parameter_datetime()
+        {
+            var target = new CommandBuilder(new SqlConnection());
+            target.Append(@"SELECT elephant ");
+            target.Append(@"FROM africa ");
+            target.Append(@"WHERE sex = 'male' ");
+
+            target.AppendWithParameterWhenNotNull("AND born > @date", "@date", new DateTime(2016, 10, 03));
+
+            var actual = (SqlParameter)target.Parameters[0];
+
+            Assert.Equal("@date", actual.ParameterName);
+            Assert.Equal(new DateTime(2016, 10, 03), actual.Value);
+        }
+
+        [Fact]
+        public void AppendParameterPhraseWhenNotNull_check_parameter_string()
+        {
+            var target = new CommandBuilder(new SqlConnection());
+            target.Append(@"SELECT elephant ");
+            target.Append(@"FROM africa ");
+            target.Append(@"WHERE sex = 'male' ");
+
+            target.AppendWithParameterWhenNotNull("AND name like @name", "@name", "Jumbo");
+
+            var actual = (SqlParameter)target.Parameters[0];
+
+            Assert.Equal("@name", actual.ParameterName);
+            Assert.Equal("Jumbo", actual.Value);
+        }
+
+        [Fact]
+        public void AppendParameterPhraseWhenNotNull_check_parameter_null()
+        {
+            var target = new CommandBuilder(new SqlConnection());
+            target.Append(@"SELECT elephant ");
+            target.Append(@"FROM africa ");
+            target.Append(@"WHERE sex = 'male' ");
+
+            target.AppendWithParameterWhenNotNull("AND born > @date", "@date", (object)null);
+
+            var actual = target.Parameters;
+
+            Assert.Empty(actual);
+        }
+
+        [Fact]
+        public void AddParameter()
+        {
+            var target = new CommandBuilder(new SqlConnection());
+            target.Append(@"SELECT elephant ");
+            target.Append(@"FROM africa");
+            target.Append(@" WHERE sex = @sex");
+
+            target.AddParameter("@sex", "male");
+
+            var actual = (SqlParameter)target.Parameters[0];
+
+            Assert.Equal("@sex", actual.ParameterName);
+            Assert.Equal("male", actual.Value);
+        }
+
+        [Fact]
+        public void AppendParameterPhraseWhenNotNull_list_check_command_text()
+        {
+            var target = new CommandBuilder(new SqlConnection());
+            target.Append(@"SELECT elephant ");
+            target.Append(@"FROM africa ");
+            target.Append(@"WHERE sex = 'male' ");
+
+            target.AppendWithParameterWhenNotNull("AND id in (@idlist)", "@idlist", new long[] { 1, 3, 5 });
+
+            var expected = @"SELECT elephant FROM africa WHERE sex = 'male' AND id in (1, 3, 5)";
+            var actual = target.CommandText;
+
+            Assert.Equal(expected, actual);
+        }
+
+
+        [Fact]
+        public void AppendParameterPhraseWhenNotNull_list_check_parameter_null()
+        {
+            var target = new CommandBuilder(new SqlConnection());
+            target.Append(@"SELECT elephant ");
+            target.Append(@"FROM africa ");
+            target.Append(@"WHERE sex = 'male' ");
+
+            target.AppendWithParameterWhenNotNull("AND id in (@idlist)", "@idlist", (long[])null);
+
+            var actual = target.Parameters;
+
+            Assert.Empty(actual);
+        }
+    }
+}
---
+++ src/Trichterwolke.DataExtentions/CommandBuilder.cs
@@ -0,0 +1,309 @@
+﻿namespace Trichterwolke.DataExtentions
+{
+    using System;
+    using System.Text;
+    using System.Data;
+    using System.Linq;
+    using System.Collections.Generic;
+
+
+    /// <summary>
+    /// Ein Komposition eines StringBuilder- und SqlCommand-Objekts um mittels eines 
+    /// Methodenaufrufs einen optionalen SQL-Ausdruck sowie den dazugehörigen parameter hinzuzufügen.
+    /// </summary>
+    public class CommandBuilder : IDisposable, IDbCommand
+    {
+        private readonly IDbCommand command;
+        private readonly StringBuilder builder = new StringBuilder();
+
+        /// <summary>
+        /// Erzeigt ein neues Objekt
+        /// </summary>
+        /// <param name="connection">SQL-Verbindungsobjekt</param>
+        public CommandBuilder(IDbConnection connection)
+        {
+            this.command = connection.CreateCommand();
+            this.command.Connection = connection;
+        }
+
+        /// <summary>
+        /// Fügt einen Parameter hinzu
+        /// </summary>
+        /// <param name="parameterName">Name des Parameters</param>
+        /// <param name="parameterValue">Parameterwert</param>
+        public void AddParameter<T>(string parameterName, T parameterValue)
+        {
+            var parameter = this.command.CreateParameter();
+            parameter.Value = parameterValue;
+            parameter.ParameterName = parameterName;
+            this.command.Parameters.Add(parameter);
+        }
+
+        /// <summary>
+        /// Fügt den übergebenen Text am Ende des Commands an.
+        /// </summary>
+        /// <param name="text">Der Text den Angefügt wird./param>
+        public void Append(string text)
+        {
+            builder.Append(text);
+        }
+
+        /// <summary>
+        /// Fügt den übergebenen Text am Ende des Commands an.
+        /// </summary>
+        /// <param name="append">Gibt an, ob der Text angehängt wird oder nicht.</param>
+        /// <param name="text">Der Text den Angefügt wird./param>
+        public void Append(string text, bool append)
+        {
+            if (append)
+            {
+                builder.Append(text);
+            }
+        }
+
+        /// <summary>
+        /// Wenn der übergebene Parameter nicht null ist, dann wird der übergebene Text angehängt 
+        /// und der Parameter der SqlConnection hinzugefügt.
+        /// </summary>
+        /// <param name="text">Der Text den Angefügt wird.</param>
+        /// <param name="parameterName">Name des Parameters</param>
+        /// <param name="parameterValue">Parameterwert</param>
+        public void AppendWithParameterWhenNotNull(string text, string parameterName, object parameterValue)
+        {
+            AppendWithParameter(text, parameterName, parameterValue, parameterValue != null);
+        }
+
+        /// <summary>
+        /// Wenn der übergebene Parameter nicht null ist, dann wird der übergebene Text angehängt 
+        /// und der Parameter als kommaseparierte Liste eingefügt.
+        /// </summary>
+        /// <param name="text">Der Text den Angefügt wird.</param>
+        /// <param name="parameterName">Name des Parameters</param>
+        /// <param name="parameterValue">Parameterwert</param>
+        public void AppendWithParameterWhenNotNull(string text, string parameterName, IEnumerable<byte> parameterValue)
+        {
+            AppendWithParameter<byte>(text, parameterName, parameterValue, parameterValue != null);
+        }
+
+        /// <summary>
+        /// Wenn der übergebene Parameter nicht null ist, dann wird der übergebene Text angehängt 
+        /// und der Parameter als kommaseparierte Liste eingefügt.
+        /// </summary>
+        /// <param name="text">Der Text den Angefügt wird.</param>
+        /// <param name="parameterName">Name des Parameters</param>
+        /// <param name="parameterValue">Parameterwert</param>
+        public void AppendWithParameterWhenNotNull(string text, string parameterName, IEnumerable<int> parameterValue)
+        {
+            AppendWithParameter<int>(text, parameterName, parameterValue, parameterValue != null);
+        }
+
+        /// <summary>
+        /// Wenn der übergebene Parameter nicht null ist, dann wird der übergebene Text angehängt 
+        /// und der Parameter als kommaseparierte Liste eingefügt.
+        /// </summary>
+        /// <param name="text">Der Text den Angefügt wird.</param>
+        /// <param name="parameterName">Name des Parameters</param>
+        /// <param name="parameterValue">Parameterwert</param>
+        public void AppendWithParameterWhenNotNull(string text, string parameterName, IEnumerable<short> parameterValue)
+        {
+            AppendWithParameter<short>(text, parameterName, parameterValue, parameterValue != null);
+        }
+
+        /// <summary>
+        /// Wenn der übergebene Parameter nicht null ist, dann wird der übergebene Text angehängt 
+        /// und der Parameter als kommaseparierte Liste eingefügt.
+        /// </summary>
+        /// <param name="text">Der Text den Angefügt wird.</param>
+        /// <param name="parameterName">Name des Parameters</param>
+        /// <param name="parameterValue">Parameterwert</param>
+        public void AppendWithParameterWhenNotNull(string text, string parameterName, IEnumerable<long> parameterValue)
+        {
+            AppendWithParameter<long>(text, parameterName, parameterValue, parameterValue != null);
+        }
+
+        /// <summary>        
+        /// Wenn append true ist, dann wird der übergebene Text angehängt 
+        /// und der Parameter der SqlParemterConnection hinzugefügt.
+        /// </summary>
+        /// <param name="text">Der Text den Angefügt wird.</param>
+        /// <param name="parameterName">Name des Parameters</param>
+        /// <param name="parameterValue">Parameterwert</param>
+        /// <param name="append">Gibt an, ob der Parameter und der Text angehängt wird oder nicht.</param>
+        public void AppendWithParameter(string text, string parameterName, object parameterValue, bool append)
+        {
+            if (append)
+            {
+                this.builder.Append(text);
+                var parameter = this.command.CreateParameter();
+                parameter.Value = parameterValue;
+                parameter.ParameterName = parameterName;
+                this.command.Parameters.Add(parameter);
+            }
+        }
+
+        /// <summary>        
+        /// Wenn append true ist, dann wird der übergebene Text angehängt und der übergebene Parameter
+        /// als kommaseparierte Liste eingefügt.
+        /// </summary>
+        /// <param name="text">Der Text den Angefügt wird.</param>
+        /// <param name="parameterName">Name des Parameters</param>
+        /// <param name="parameterValue">Parameterwert</param>
+        /// <param name="append">Gibt an, ob der Parameter und der Text angehängt wird oder nicht.</param>
+        private void AppendWithParameter(string text, string parameterName, IEnumerable<byte> parameterValue, bool append)
+        {
+            AppendWithParameter<byte>(text, parameterName, parameterValue, append);
+        }
+
+        /// <summary>        
+        /// Wenn append true ist, dann wird der übergebene Text angehängt und der übergebene Parameter
+        /// als kommaseparierte Liste eingefügt.
+        /// </summary>
+        /// <param name="text">Der Text den Angefügt wird.</param>
+        /// <param name="parameterName">Name des Parameters</param>
+        /// <param name="parameterValue">Parameterwert</param>
+        /// <param name="append">Gibt an, ob der Parameter und der Text angehängt wird oder nicht.</param>
+        private void AppendWithParameter(string text, string parameterName, IEnumerable<short> parameterValue, bool append)
+        {
+            AppendWithParameter<short>(text, parameterName, parameterValue, append);
+        }
+
+        /// <summary>        
+        /// Wenn append true ist, dann wird der übergebene Text angehängt und der übergebene Parameter
+        /// als kommaseparierte Liste eingefügt.
+        /// </summary>
+        /// <param name="text">Der Text den Angefügt wird.</param>
+        /// <param name="parameterName">Name des Parameters</param>
+        /// <param name="parameterValue">Parameterwert</param>
+        /// <param name="append">Gibt an, ob der Parameter und der Text angehängt wird oder nicht.</param>
+        private void AppendWithParameter(string text, string parameterName, IEnumerable<int> parameterValue, bool append)
+        {
+            AppendWithParameter<int>(text, parameterName, parameterValue, append);
+        }
+
+        /// <summary>        
+        /// Wenn append true ist, dann wird der übergebene Text angehängt und der übergebene Parameter
+        /// als kommaseparierte Liste eingefügt.
+        /// </summary>
+        /// <param name="text">Der Text den Angefügt wird.</param>
+        /// <param name="parameterName">Name des Parameters</param>
+        /// <param name="parameterValue">Parameterwert</param>
+        /// <param name="append">Gibt an, ob der Parameter und der Text angehängt wird oder nicht.</param>
+        private void AppendWithParameter(string text, string parameterName, IEnumerable<long> parameterValue, bool append)
+        {
+            AppendWithParameter<long>(text, parameterName, parameterValue, append);
+        }
+
+
+        private void AppendWithParameter<T>(string text, string parameterName, IEnumerable<T> parameterValue, bool append)
+        {
+            if (append && text != null && parameterValue != null)
+            {
+                this.builder.Append(text.Replace(parameterName, string.Join(", ", parameterValue)));
+                var parameter = this.command.CreateParameter();
+                parameter.Value = parameterValue;
+                parameter.ParameterName = parameterName;
+                this.command.Parameters.Add(parameter);
+            }
+        }
+
+        private void SetCommandText()
+        {
+            this.command.CommandText = this.builder.ToString();
+        }
+
+        public IDataReader ExecuteReader()
+        {
+            SetCommandText();
+            return this.command.ExecuteReader();
+        }
+
+        public IDataReader ExecuteReader(CommandBehavior behavior)
+        {
+            SetCommandText();
+            return this.command.ExecuteReader(behavior);
+        }
+
+        public object ExecuteScalar()
+        {
+            SetCommandText();
+            return this.command.ExecuteScalar();
+        }
+
+        public int ExecuteNonQuery()
+        {
+            SetCommandText();
+            return this.command.ExecuteNonQuery();
+        }
+
+        public IDataParameterCollection Parameters
+        {
+            get { return this.command.Parameters; }
+        }
+
+        public IDbConnection Connection
+        {
+            get { return this.command.Connection; }
+            set { this.command.Connection = value; }
+        }
+
+        public IDbTransaction Transaction
+        {
+            get { return this.command.Transaction; }
+            set { this.command.Transaction = value; }
+        }
+
+        public string CommandText
+        {
+            get { return this.builder.ToString(); }
+            set
+            {
+                this.builder.Clear();
+                this.builder.Append(value);
+            }
+        }
+
+        public int CommandTimeout
+        {
+            get { return this.command.CommandTimeout; }
+            set { this.command.CommandTimeout = value; }
+        }
+
+        public CommandType CommandType
+        {
+            get { return this.command.CommandType; }
+            set { this.command.CommandType = value; }
+        }
+
+        public UpdateRowSource UpdatedRowSource
+        {
+            get { return this.command.UpdatedRowSource; }
+            set { this.command.UpdatedRowSource = value; }
+        }
+
+        /// <summary>
+        /// Gibt die Resouces des SqlCommand wieder frei.
+        /// </summary>
+        public void Dispose()
+        {
+            if (command != null)
+            {
+                command.Dispose();
+            }
+        }
+
+        public void Prepare()
+        {
+            this.command.Prepare();
+        }
+
+        public void Cancel()
+        {
+            this.command.Cancel();
+        }
+
+        public IDbDataParameter CreateParameter()
+        {
+            return this.command.CreateParameter();
+        }
+    }
+}
